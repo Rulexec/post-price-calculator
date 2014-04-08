c6d9a8eb4d673dd5e5ede01b1290ac9b3ade3758
Alexander Ruliov <rulix.exec@gmail.com>
2014-04-08 16:00:21 +0300

    added cause to BadPostCLICommandException

--- src/api/java/by/muna/moep/post/cli/BadPostCLICommandException.java
+++ src/api/java/by/muna/moep/post/cli/BadPostCLICommandException.java
@@ -4,4 +4,10 @@ public class BadPostCLICommandException extends Exception {
     public BadPostCLICommandException(String message) {
         super(message);
     }
+    public BadPostCLICommandException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public BadPostCLICommandException(Throwable cause) {
+        super(cause);
+    }
 }
