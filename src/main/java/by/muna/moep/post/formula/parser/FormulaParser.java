02b4e047256ad803c0e4edc0705ca7e86bdc5437
Alexander Ruliov <rulix.exec@gmail.com>
2014-04-12 00:28:41 +0300

    Start of parser dev.

---
+++ src/main/java/by/muna/moep/post/formula/parser/FormulaParser.java
@@ -0,0 +1,19 @@
+package by.muna.moep.post.formula.parser;
+
+import by.muna.moep.post.formula.IFormulaBuilder;
+
+public class FormulaParser implements IFormulaParser {
+    public FormulaParser() {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public void feed(CharSequence chars) throws BadFormulaSyntaxException {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public IFormulaBuilder end() throws BadFormulaSyntaxException {
+        throw new RuntimeException("Not implemented yet.");
+    }
+}
