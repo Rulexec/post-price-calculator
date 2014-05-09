29864a67560782081f8b1d055a71e3f3e60a17fb
Alexander Ruliov <rulix.exec@gmail.com>
2014-05-09 19:34:42 +0300

    CLI tests now use factory

---
+++ src/test/java/by/muna/moep/post/cli/tests/factories/PostCLIFactory.java
@@ -0,0 +1,10 @@
+package by.muna.moep.post.cli.tests.factories;
+
+import by.muna.moep.post.IPost;
+import by.muna.moep.post.cli.IPostCLI;
+
+public class PostCLIFactory {
+    public static IPostCLI createPostCLI(IPost post) {
+        throw new RuntimeException("PostCLI not implemented yet.");
+    }
+}
